52fbe72e9ea0878b6a1fafcd5ec8168461f6d741
Kaholaz <vkbugge@hotmail.com>
2023-04-13 05:07:39 +0200

    initial algorithm

---
+++ main.py
@@ -0,0 +1,133 @@
+from dataclasses import field, dataclass
+
+
+@dataclass
+class Agent:
+    name: str
+    received: int = 0
+    payed: int = 0
+
+    # Positive total means you owe money, negative means you are owed money.
+    @property
+    def total(self) -> int:
+        return self.received - self.payed
+
+
+@dataclass
+class Expense:
+    payee: str
+    liabilities: dict[str, int] = field(default_factory=dict)
+
+    @property
+    def total(self) -> int:
+        return sum(self.liabilities.values())
+
+
+@dataclass
+class Transfer:
+    payer: Agent
+    payee: Agent
+    amount: int
+
+
+@dataclass
+class Settlement:
+    transfers: list[Transfer] = field(default_factory=list)
+
+    agent_dict: dict[str, Agent] = field(default_factory=dict)
+    unresolved: list[Agent] = field(default_factory=list)
+
+    def insert_or_get_agent(self, agent_name: str) -> Agent:
+        if agent_name in self.agent_dict:
+            agent = self.agent_dict[agent_name]
+        else:
+            agent = Agent(agent_name)
+            self.agent_dict[agent_name] = agent
+        return agent
+
+
+def generate_settlement(expenses: list[Expense]) -> Settlement:
+    settlement = Settlement()
+
+    # Construct agent nodes
+    agents_dict = dict[str, Agent]
+    for expense in expenses:
+        for agent_names, amount in expense.liabilities.items():
+            agent = settlement.insert_or_get_agent(agent_names)
+            agent.received += amount
+        agent = settlement.insert_or_get_agent(expense.payee)
+        agent.payed += expense.total
+
+    settlement.unresolved: list[Agent] = sorted(
+        filter(lambda a: a.total != 0, settlement.agent_dict.values()),
+        key=lambda a: a.total,
+    )
+    while len(settlement.unresolved):
+        assert len(settlement.unresolved) != 1  # Can i assume this to be true?
+        payee = settlement.unresolved[0]
+        payer = settlement.unresolved[-1]
+
+        # Calculate transfer
+        amount = min(-payee.total, payer.total)
+        transfer = Transfer(payer, payee, amount)
+        settlement.transfers.append(transfer)
+
+        payee.received += amount
+        payer.payed += amount
+
+        # Remove or move payer
+        if payer.total == 0:
+            settlement.unresolved.pop()
+        else:
+            i = -1
+            while payer.total < settlement.unresolved[i - 1].total:
+                settlement.unresolved[i], settlement.unresolved[i - 1] = (
+                    settlement.unresolved[i - 1],
+                    settlement.unresolved[i],
+                )
+                i -= 1
+
+        if payee.total == 0:
+            settlement.unresolved.pop(0)
+        elif len(settlement.unresolved):
+            i = 0
+            while settlement.unresolved[i + 1].total < payee.total:
+                settlement.unresolved[i + 1], settlement.unresolved[1] = (
+                    settlement.unresolved[i],
+                    settlement.unresolved[i + 1],
+                )
+                i += 1
+
+    return settlement
+
+
+if __name__ == "__main__":
+    expenses = [
+        Expense(
+            "Bjørnar",
+            {
+                "Sebastian": 1,
+                "Mikkel": 3,
+                "Emma": 2,
+                "Bjørnar": 3,
+            },
+        ),
+        Expense(
+            "Mikkel",
+            {
+                "Mikkel": 2,
+                "Sebastian": 1,
+            },
+        ),
+        Expense(
+            "Emma",
+            {
+                "Emma": 1,
+                "Mikkel": 2,
+                "Sebastian": 5,
+            },
+        ),
+    ]
+
+    settlement = generate_settlement(expenses)
+    print("\n".join(map(str, settlement.transfers)))
